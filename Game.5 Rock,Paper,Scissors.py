5ac7854cf21a9cbb8d90e12235053569db0f0208
Leo Wong <145821779+Leo-WongCN@users.noreply.github.com>
2024-04-09 14:00:00 +0800

    Update Game.5 Rock,Paper,Scissors.py

--- Game.5 Rock,Paper,Scissors.py
+++ Game.5 Rock,Paper,Scissors.py
@@ -1,46 +1,5 @@
-# # Game: Rock paper scissors
-# # if draw not count
-# # 3 rounds each game, win 2 rounds
+# Game: Rock paper scissors
 
-# options = ['rock','paper','scissors']
-# i_win = 0
-# computer_win = 0
-
-# import random
-
-# def game():
-#     global i_win
-#     global computer_win
-    
-#     my = input('INPUT rock,paper,scissors:').lower()
-#     while my not in options:
-#         my = input('Please input rock,paper,scissors:').lower()
-#     computer = random.choice(options)
-#     print(f'Computer plays the {computer.upper()}')
-    
-#     if my == computer:
-#         print('Draw')
-#     elif (my == 'rock' and computer == 'scissors') or (my == 'paper' and computer == 'rock') or (my == 'scissors' and computer == 'paper'):
-#         print('You Win')
-#         i_win += 1
-#     else:
-#         print('Computer Win')
-#         computer_win += 1
-
-#     print(f'You get {i_win}; computer gets {computer_win}') 
-#     return i_win, computer_win
-
-# while i_win < 2 and computer_win <2:
-#     game()
-    
-# if i_win == 2:
-#     print(f'\nCongratulations')
-# else:
-#     print(f'\nComputer Win.')  
-
-
-
-# v2
 import random
 
 def game():
